da4c5d501b9cdbb3e358f199e260ec1158fa478f
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2017-09-23 06:42:28 -0500

    holy balls

--- web/app.js
+++ web/app.js
@@ -1,1 +1,42 @@
-console.log('I am web!')
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+// Configuration
+const port = process.env.PORT || 80
+mongoose.connect(process.env.MONGO_URI)
+
+const Professional = mongoose.model('Professional', {
+  id: Number,
+  slug: String,
+  data: {
+    name: {
+      first: String,
+      middle: String,
+      last: String
+    },
+    biography: {
+      main: String,
+      additional: String
+    }
+  }
+})
+
+// App initialization
+const app = express()
+app.use(morgan('tiny'))
+
+// Routes
+app.get('/', (req, res) =>
+  getProfessionals()
+    .then(professionals => res.json(professionals))
+    .catch(reason => res.json({ error: reason }))
+)
+
+const getProfessionals = () =>
+  Professional.find().lean().exec()
+
+// Start app
+app.listen(port, () =>
+  console.info(`Worker ready at http://localhost:${port}`)
+)
